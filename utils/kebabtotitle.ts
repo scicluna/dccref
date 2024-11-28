75f15a8edb78139d127c7ce3ee595adb49a29fd0
scicluna <88948700+scicluna@users.noreply.github.com>
2024-11-28 12:43:46 -0500

    fixed some broken json and added a guard to kebobtitle

--- utils/kebabtotitle.ts
+++ utils/kebabtotitle.ts
@@ -1,4 +1,7 @@
 export function kebabToTitle(kebab: string): string {
+  if (!kebab){
+    return 'error'
+  }
     return kebab
       .split('-') // Split the kebab-case string into words
       .map(word => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize each word
